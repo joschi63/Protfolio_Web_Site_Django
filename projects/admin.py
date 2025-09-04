4b85958579669a7d1ceccd3f489245a7e7e21d3d
Johann Schneider <joschi.dev7@gmail.com>
2025-09-04 11:04:22 +0200

    some little changes, added admin and created templates for projects app

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
 
 # Register your models here.
